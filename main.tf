e49a078437794f65d2e03068118b1dc3bba7eb0a
Sharifoff <efqanserifov@gmail.com>
2023-01-16 13:12:31 +0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
 resource "local_file" "state" {
-  filename  = "/home/cloudshell-user/${var.remote-state}"
+  filename  = "${var.remote-state}"
   content = "This configuration uses ${var.remote-state} state"
-
 }
